d100db0764399328280a54ee283afda13d6a5d22
Vincent DOUANT <vincent@edupad.com>
2018-07-05 23:54:12 +0200

    Improve APIManager response error handling by returning different error

--- Forecast/Code/Network/APIManager.swift
+++ Forecast/Code/Network/APIManager.swift
@@ -8,6 +8,16 @@
 
 import Foundation
 
+enum APIManagerErrorDomain : String {
+    case fetchWeather = "APIManager.fetchWeather"
+}
+
+enum APIManagerFetchWeatherErrorCode : Int {
+    case noRequestState
+    case genericError
+    case emptyData
+}
+
 class APIManager {
     static let sharedInstance = APIManager()
     private let token = "ARtXQFUrUXNWe1ViAHZSewdvUmcNewcgAX1RMgtuUC0BalQ1B2dRN18xAXwEK1BmVnsBYgw3BzcHbFEpCnhUNQFrVztVPlE2VjlVMAAvUnkHKVIzDS0HIAFqUTQLeFAyAWBUNAd6UTpfMgF9BDZQZVZsAX4MLAc%2BB2FRNQpnVD8BYlcyVT5RMlY5VSgAL1JjB2ZSMQ1nBz0BN1E%2FC2RQNgFkVDMHN1EzXzkBfQQyUGRWZAFgDDMHOwdjUT4KeFQoARtXQFUrUXNWe1ViAHZSewdhUmwNZg%3D%3D"
@@ -18,20 +28,32 @@ class APIManager {
         let url = URL(string: "http://www.infoclimat.fr/public-api/gfs/json?_ll=48.85341,2.3488&_auth=\(token)&_c=cfe4aea3bd030815d5365909bbf7e3c5")
         let urlSession = URLSession(configuration: .default)
         let task = urlSession.dataTask(with: url!) { (data, response, error) in
-            // TODO: better code handling
             guard let response = response as? HTTPURLResponse, response.statusCode == 200, let data = data else {
                 errorBlock?(error)
                 return
             }
             do {
-                if let dict = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any],
-                    let requestState = dict["request_state"] as? Int, requestState == 200 {
-                    successBlock(dict)
+                if let dict = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] {
+                    if let requestState = dict["request_state"] as? Int {
+                        if requestState == 200 {
+                            successBlock(dict)
+                        } else {
+                            errorBlock?(self.newFetchWeatherError(code: .genericError))
+                        }
+                    } else {
+                        errorBlock?(self.newFetchWeatherError(code: .noRequestState))
+                    }
+                }  else {
+                    errorBlock?(self.newFetchWeatherError(code: .emptyData))
                 }
             } catch let error {
                 errorBlock?(error)
             }
         }
         task.resume()
+    }
+
+    private func newFetchWeatherError(code: APIManagerFetchWeatherErrorCode) -> Error {
+        return NSError(domain: APIManagerErrorDomain.fetchWeather.rawValue, code: code.rawValue, userInfo: nil)
     }
 }
